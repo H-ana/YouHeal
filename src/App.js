d3a5aa8a7618e66df8a4a01dc66f1fd44e3caeff
Rahul v s <rahulvs037@gmail.com>
2022-12-07 22:00:31 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from './components/footer/footer';
 import Home from './pages/homepage/homepage';
 import OurServices from './pages/ourservices/ourservices';
 import AboutUs from './pages/aboutus/aboutus';
+import Dept from './pages/departments/departments';
+import Contactus from './pages/contactus/contactus';
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path='/' element={<><Navbar/><Home/><Footer/></>}/>
           <Route path='/ourservices' element={<><Navbar/><OurServices/><Footer/></>}/>
           <Route path='/aboutus' element={<><Navbar/><AboutUs/><Footer/></>}/>
+          <Route path='/departments' element={<><Navbar/><Dept/><Footer/></>}/>
+          <Route path='/contactus' element={<><Navbar/><Contactus/><Footer/></>}/>
         </Routes>
     </Router>
   );
