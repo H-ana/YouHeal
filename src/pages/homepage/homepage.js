5809e1fd153ce6ee121f351941af255bfba8650e
Hana Shelbin <hanashelbin@gmail.com>
2022-12-07 08:13:31 +0530

    homepage completed

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -15,23 +15,23 @@ function Home(){
                 </div>
                 <div className='bottomright'>
                 <h2>Welcome To YouHeal Central Hospital</h2>
-                <h4>We provide experienced doctors, state of the art technology, and the highest level of patient care and treatment. Our COE’s ensure that every aspect of your care is seamless and the team of experts work together to provide the care you need.
+                <h4 className='decr'>We provide experienced doctors, state of the art technology, and the highest level of patient care and treatment. Our COE’s ensure that every aspect of your care is seamless and the team of experts work together to provide the care you need.
                 </h4>
                 <div className='homebox'>
                     <div className='homeboxitem'>
-                    <h4 className='tickicon'><IoMdCheckmarkCircleOutline/></h4>
+                    <h3 className='tickicon'><IoMdCheckmarkCircleOutline/></h3>
                     <h4>5000+ ready beds</h4>
                     </div>
                     <div className='homeboxitem'>
-                    <h4 className='tickicon'><IoMdCheckmarkCircleOutline/></h4>
+                    <h3 className='tickicon'><IoMdCheckmarkCircleOutline/></h3>
                     <h4>20+ icu solution</h4>
                     </div>
                     <div className='homeboxitem'>
-                    <h4 className='tickicon'><IoMdCheckmarkCircleOutline/></h4>
+                    <h3 className='tickicon'><IoMdCheckmarkCircleOutline/></h3>
                     <h4>Probably the Largest Hospital</h4>
                     </div>
                     <div className='homeboxitem'>
-                    <h4 className='tickicon'><IoMdCheckmarkCircleOutline/></h4>
+                    <h3 className='tickicon'><IoMdCheckmarkCircleOutline/></h3>
                     <h4>20/7 Open Support</h4>
                     </div>
                 </div>
