dc7ee43f5cf603931e2c14355b6d5b6eea4fce69
Hana Shelbin <hanashelbin@gmail.com>
2022-12-07 07:36:29 +0530

    error corrected

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
+import Home from './pages/homepage/homepage';
+import OurServices from './pages/ourservices/ourservices';
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
         <Routes>
-          <Route path='/' element={<><Navbar/><Footer/></>}/>
+          <Route path='/' element={<><Navbar/><Home/><Footer/></>}/>
+          <Route path='/ourservices' element={<><Navbar/><OurServices/><Footer/></>}/>
         </Routes>
     </Router>
   );
