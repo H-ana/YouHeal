544252c07d6dd300c22a1d32e191e5083f8ce5cb
Hana Shelbin <hanashelbin@gmail.com>
2022-12-08 08:54:00 +0530

    anaesthesia completed

--- src/pages/anaesthesia/anaesthesia.js
+++ src/pages/anaesthesia/anaesthesia.js
@@ -5,7 +5,9 @@ import './anaesthesia.css';
 function Anas(){
     return(<div className='an2'>
         <div className='an1'>
-            <img className='Anasimg'src={Ana}/>
+            <div><img className='Anasimg'src={Ana}/>
+            <h1>Anaesthesia</h1>
+            <p>Anaesthesia refers to a controlled, near-zero-sensation state of the human body that is required for surgical / medical purposes. The branch of Surgical Anesthesia for OBG Surgeries relies on multiple pillars / purpose-driven procedures - Paralysis (Relaxation of the body muscles), Analgesia (pain prevention procedure), Amnesia (memory removal), and / or unconsciousness. ‘Anaesthetized’ refers to the state of being under an anaesthetic drug. Anaesthesia helps a number of painless surgeries, and medical procedures that may otherwise cause enormous pain and suffering to the patient. In certain cases, a surgery without anaesthesia may even be technically impossible. </p></div>
             <div className='doc-grp'>
             <img className='Docimg'src={Doc} />
             <h4>Dr Dikshitha Chetty</h4>
@@ -13,8 +15,7 @@ function Anas(){
             <center><button className='view-button'>View full profile</button></center>
             </div>
             </div>
-            <h1>Anaesthesia</h1>
-            <p>Anaesthesia refers to a controlled, near-zero-sensation state of the human body that is required for surgical / medical purposes. The branch of Surgical Anesthesia for OBG Surgeries relies on multiple pillars / purpose-driven procedures - Paralysis (Relaxation of the body muscles), Analgesia (pain prevention procedure), Amnesia (memory removal), and / or unconsciousness. ‘Anaesthetized’ refers to the state of being under an anaesthetic drug. Anaesthesia helps a number of painless surgeries, and medical procedures that may otherwise cause enormous pain and suffering to the patient. In certain cases, a surgery without anaesthesia may even be technically impossible. </p>
+            
             </div>
     );
 }
