d972696e4c62e5541bc97096d9733379b4ab7301
Rahul v s <rahulvs037@gmail.com>
2022-12-08 07:26:47 +0530

    gallery, input change

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import image1 from '../../images/gallery1.png';
+import image2 from '../../images/gallery2.png';
+import image3 from '../../images/gallery3.png';
+import image4 from '../../images/gallery4.png';
+import image5 from '../../images/gallery5.png';
+import image6 from '../../images/gallery6.png';
+import image7 from '../../images/gallery7.png';
+import image8 from '../../images/gallery8.png';
 import './gallery.css';
-import {data} from'../../data';
 function Gallery() {
-  const [gallery, setgallery] = React.useState(data);
   return (
-    <div>
-      <>
-      {
-        gallery.map((item) =>{
-        const {image} = item;
-        return(
-        <div className='item'>
-          <img src={image}/>
-        </div>
-      )
-    })
-  }
-  </>
-  </div>
+    <div className='gallery-main'>
+      <h1>Gallery</h1>
+    <div className='gallery-container'>
+      <img src={image1}/>
+      <img src={image2}/>
+      <img src={image3}/>
+      <img src={image4}/>
+      <img src={image5}/>
+      <img src={image6}/>
+      <img src={image7}/>
+      <img src={image8}/>
+    </div>
+    </div>
   )
 }
 
